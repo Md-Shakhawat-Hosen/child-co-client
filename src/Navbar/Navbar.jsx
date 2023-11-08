ff2740aa5cf40d43b56c1b95199d077ef57396d1
Md-Shakhawat-Hosen <shakhawat.hosen.nsu@gmail.com>
2023-11-08 20:57:00 +0600

    added logout btn, if user click that btn user singout from login

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -76,7 +76,9 @@ const Navbar = () => {
             </div>
             <div className="flex items-center justify-center">
               <img className="w-[80px]" src={logo} alt="" />
-              <a className=" text-xl font-bold">Child<span className="text-orange-400">Co</span></a>
+              <a className=" text-xl font-bold">
+                Child<span className="text-orange-400">Co</span>
+              </a>
             </div>
           </div>
           <div className="navbar-center hidden lg:flex">
@@ -109,29 +111,32 @@ const Navbar = () => {
           </div>
           <div className="navbar-end">
             {user?.email ? (
-              <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                  <div className="w-10 rounded-full">
-                    <img src={user?.photoURL} referrerPolicy="no-referrer" />
-                  </div>
-                </label>
-                <ul
-                  tabIndex={0}
-                  className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-                >
-                  <li>
-                    <a className="justify-between">
-                      {user?.displayName}
-                      {/* <span className="badge">New</span> */}
-                    </a>
-                  </li>
-
-                  <li className="">
-                    <button onClick={handleLogout} className="bg-gray-300 ">
+              <div className="flex gap-6">
+                <div className="dropdown dropdown-end">
+                  <label
+                    tabIndex={0}
+                    className="btn btn-ghost btn-circle avatar"
+                  >
+                    <div className="w-10 rounded-full">
+                      <img src={user?.photoURL} referrerPolicy="no-referrer" />
+                    </div>
+                  </label>
+                  <ul
+                    tabIndex={0}
+                    className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                  >
+                    <li>
+                      <a className="justify-between">
+                        {user?.displayName}
+                        {/* <span className="badge">New</span> */}
+                      </a>
+                    </li>
+                  </ul>
+                  
+                </div>
+                <button onClick={handleLogout} className="btn" >
                       Logout
                     </button>
-                  </li>
-                </ul>
               </div>
             ) : (
               <NavLink to="/login" className="btn">
